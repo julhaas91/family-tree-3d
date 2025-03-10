e05402afe2ca74b7ff2e753a7d9f7373fc198dad
Julius Haas <juliushaas91@gmail.com>
2025-03-10 22:30:33 +0100

    chore: minor changes

--- taskfile.sh
+++ taskfile.sh
@@ -10,12 +10,16 @@ create() {
 
   echo "Creating the bucket..."
   gsutil mb gs://${BUCKET_NAME}
+
   echo "Making bucket publicly readable..."
   gsutil iam ch allUsers:objectViewer gs://${BUCKET_NAME}
+  
   echo "Setting the bucket to host a static website..."
   gsutil web set -m index.html -e 404.html gs://${BUCKET_NAME}
+  
   echo "Building the application..."
   gsutil -m cp -r ./dist/* gs://${BUCKET_NAME}/
+  
   echo "Successfully created the cloud storage bucket and set it to host a static website!"
   echo "The website is available at: https://storage.googleapis.com/${BUCKET_NAME}/index.html"
 }
@@ -27,11 +31,18 @@ update() {
     exit 1
   fi
 
+
+  echo "Deleting all files from the bucket: ${BUCKET_NAME}..."
+  gsutil -m rm -r gs://${BUCKET_NAME}/*
+
   echo "Building the application..."
   npm run build
+
   echo "Deploying the application to Google Cloud Storage..."
   gsutil -m cp -r ./dist/* gs://${BUCKET_NAME}/
+
   echo "Successfully built and deployed the application to Google Cloud Storage"
+  echo "The website is available at: https://storage.googleapis.com/${BUCKET_NAME}/index.html"
 }
 
 show_url() {
